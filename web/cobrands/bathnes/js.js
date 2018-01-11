0dd12674a48a2da122d26ca017d910ab38d2db96
Dave Arter <davea@mysociety.org>
2018-01-11 20:48:35 +0000

    [BANES] Add LLPG asset layer (requires ‘LLPG’ category for now)

--- web/cobrands/bathnes/js.js
+++ web/cobrands/bathnes/js.js
@@ -4,34 +4,49 @@ if (!fixmystreet.maps) {
     return;
 }
 
-fixmystreet.maps.banes_map_options = function(typename, category, item) {
-    return {
-        http_options: {
-            url: "https://isharemapstest.bathnes.gov.uk/getows.ashx",
-            params: {
-                mapsource: "BathNES/WFS",
-                SERVICE: "WFS",
-                VERSION: "1.1.0",
-                REQUEST: "GetFeature",
-                TYPENAME: typename,
-                SRSNAME: "urn:ogc:def:crs:EPSG::27700"
-            }
-        },
-        asset_category: category,
-        asset_item: item,
-        asset_type: 'spot',
-        max_resolution: 2.388657133579254,
-        min_resolution: 0.5971642833948135,
-        asset_id_field: 'feature_no',
-        attributes: {
-            feature_id: 'feature_id'
-        },
-        geometryName: 'msGeometry',
-        srsName: "EPSG:27700"
-    };
+fixmystreet.maps.banes_defaults = {
+    http_options: {
+        url: "https://isharemapstest.bathnes.gov.uk/getows.ashx",
+        // url: "https://confirmdev.eu.ngrok.io/banesmaps/getows.ashx",
+        params: {
+            mapsource: "BathNES/WFS",
+            SERVICE: "WFS",
+            VERSION: "1.1.0",
+            REQUEST: "GetFeature",
+            TYPENAME: "",
+            SRSNAME: "urn:ogc:def:crs:EPSG::27700"
+        }
+    },
+    asset_category: "",
+    asset_item: "asset",
+    asset_type: 'spot',
+    max_resolution: 2.388657133579254,
+    min_resolution: 0.5971642833948135,
+    asset_id_field: 'feature_no',
+    attributes: null,
+    geometryName: 'msGeometry',
+    srsName: "EPSG:27700"
 };
 
-$(fixmystreet.add_assets(fixmystreet.maps.banes_map_options("Gritbins", "Grit bin empty", "grit bin")));
-$(fixmystreet.add_assets(fixmystreet.maps.banes_map_options("StreetLighting", "Street Light Fault", "street light")));
+
+$(fixmystreet.add_assets($.extend(true, {}, fixmystreet.maps.banes_defaults, {
+    http_options: {
+        params: {
+            TYPENAME: "Gritbins"
+        }
+    },
+    asset_category: "Grit bin issue",
+    asset_item: "grit bin"
+})));
+
+$(fixmystreet.add_assets($.extend(true, {}, fixmystreet.maps.banes_defaults, {
+    http_options: {
+        params: {
+            TYPENAME: "StreetLighting"
+        }
+    },
+    asset_category: "Street Light Fault",
+    asset_item: "street light"
+})));
 
 })();
