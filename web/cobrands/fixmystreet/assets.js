2af6488155bf71e34d6f74f8beedce4f9f74b532
Dave Arter <davea@mysociety.org>
2018-01-18 17:25:00 +0000

    fixup! Show loading spinner on map for assets as well as markers

--- web/cobrands/fixmystreet/assets.js
+++ web/cobrands/fixmystreet/assets.js
@@ -366,8 +366,8 @@ fixmystreet.add_assets = function(options) {
         }
 
         // Make sure the user knows something is happening (some asset layers can be sllooowwww)
-        asset_layer.events.register( 'loadstart', asset_layer, fixmystreet.maps.loading_spinner.show);
-        asset_layer.events.register( 'loadend', asset_layer, fixmystreet.maps.loading_spinner.hide);
+        asset_layer.events.register( 'loadstart', null, fixmystreet.maps.loading_spinner.show);
+        asset_layer.events.register( 'loadend', null, fixmystreet.maps.loading_spinner.hide);
 
         if (options.asset_category) {
             fixmystreet.map.events.register( 'zoomend', asset_layer, check_zoom_message_visibility);
